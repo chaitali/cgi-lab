f2e8c58030e89546f0d0c03e6069e3e8612db6e8
Chaitali Patel <cpatel1@cs.ualberta.ca>
2019-01-23 17:53:14 -0700

    lab3

---
+++ login.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+import cgi
+# import cgitb
+import os
+import json
+from templates import login_page, secret_page, after_login_incorrect
+from secret import username, password
+
+# cgitb.enable()
+
+print("Content-Type: text/html\n")
+# print()
+# print("<!doctype html>")
+# print(login_page())
+
+# form = cgi.FieldStorage()
+# print(form.getvalue("username"))
+# print(form.getvalue("password"))
+
+#loggin in
+# form = cgi.FieldStorage()
+# p_username = form.getvalue("username")
+# p_password = form.getvalue("password")
+
+# if p_username == username and p_password == password:
+#     print(secret_page(username, password))
+# else:
+#     print(login_page())
+
+# SET COOKIES
+form = cgi.FieldStorage()
+p_username = form.getvalue("username")
+p_password = form.getvalue("password")
+
+c_username = ""
+c_password = ""
+try:
+    cookie_string = os.environ.get("HTTP_COOKIE")
+    cookie_pairs = cookie_string.split(":")
+    for pair in cookie_pairs:
+        key, val = pair.split("=")
+        if "username" in key:
+            c_username = val
+        elif "password" in key:
+            c_password = val
+except:
+    pass
+
+if c_username and c_password:
+    print("\n\n")
+    print(secret_page(c_username,c_password))
+
+elif os.environ.get("REQUEST_METHOD", "GET") == "POST":
+    if p_username == username and p_password == password:
+        print("Set-Cookie: username={}".format(p_username))
+        print("Set-Cookie: password={}".format(p_password))
+        print(secret_page(username, password))
+    else:
+        print(after_login_incorrect())
+
+else:
+    print(login_page())
